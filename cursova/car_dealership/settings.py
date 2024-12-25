29ce64bf3c4bbb0360e55853dabb20c534eeedef
Andrew2349 <trotsiuk.andriy@lll.kpi.ua>
2024-12-25 12:48:38 +0200

    Update settings.py

--- cursova/car_dealership/settings.py
+++ cursova/car_dealership/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, STATIC_URL)]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
